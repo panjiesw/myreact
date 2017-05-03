48ed339a328fb878fa36a183a7b86ba8e05cf5cd
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2017-05-04 02:34:52 +0700

    somewhat working coverage

--- tests/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2017 Panjie Setiawan Wicaksono <panjie@panjiesw.com>
- *
- * This software is released under the MIT License.
- * https://panjiesw.mit-license.org
- */
-
-import 'babel-polyfill';
-import './web/stores/auth.spec';
